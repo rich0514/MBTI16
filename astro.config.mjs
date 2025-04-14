0e456071216d9b6b060ea77d74c492880c5d43cf
rich0514 <create.for.life.official@gmail.com>
2025-04-14 23:43:16 +0800

    Fix Astro file structure and update configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  vite: {
-    ssr: {
-      noExternal: ['axobject-query'],
-    },
-  },
+  integrations: [tailwind()],
+  site: 'https://mbti16.onrender.com',
+  base: '/',
 });
